29c178a41c3e3050dccd4bdeab113cb942b6b3bf
Tyler Kirby <tyler.kirby9398@gmail.com>
2021-11-21 15:37:09 -0600

    tried different computation of rareness

--- scripts/make_word_level_analytics.py
+++ scripts/make_word_level_analytics.py
@@ -12,9 +12,7 @@ if __name__ == "__main__":
     )
     # Compute global frequency
     total_words_corpus = full_corpus_df["count"].sum()
-    full_corpus_df["global_frequency"] = np.log(
-        total_words_corpus / full_corpus_df["count"]
-    )
+    full_corpus_df["global_frequency"] = full_corpus_df["count"] / total_words_corpus
     # Compute local frequency
     freq_table_paths = [
         "../frequency_tables/" + p
@@ -27,21 +25,20 @@ if __name__ == "__main__":
         total_words_author = df["count"].sum()
         for i, r in df.iterrows():
             if i not in local_freq:
-                local_freq[i] = (total_words_author / r["count"]) * (
-                    total_words_corpus / total_words_author
+                local_freq[i] = (r["count"] / total_words_author) * (
+                    total_words_author / total_words_corpus
                 )
             else:
-                local_freq[i] += (total_words_author / r["count"]) * (
-                    total_words_corpus / total_words_author
+                local_freq[i] += (r["count"] / total_words_author) * (
+                    total_words_author / total_words_corpus
                 )
     local_freq_df = pd.DataFrame.from_dict(
         local_freq, orient="index", columns=["local_frequency"]
     )
-    local_freq_df["local_frequency"] = np.log(local_freq_df["local_frequency"])
     analytics_df = pd.merge(
         full_corpus_df, local_freq_df, left_index=True, right_index=True
     )
-    analytics_df["rareness"] = (
+    analytics_df["rareness"] = np.log(
         analytics_df["global_frequency"] * analytics_df["local_frequency"]
     )
     analytics_df.to_csv("../analytics/word_level_rareness.csv")
