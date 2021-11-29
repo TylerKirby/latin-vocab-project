7881387bb9207f1cc2b5711ae5fba6445875103f
Tyler Kirby <tyler.kirby9398@gmail.com>
2021-11-29 16:25:44 -0600

    lexicon class and tests

--- modules/lexicon.py
+++ modules/lexicon.py
@@ -1,4 +1,8 @@
+from collections import namedtuple
 from string import punctuation
+
+import pandas as pd
+
 
 with open("../assets/diederich.txt") as f:
     diederich_vocab_raw = [l.split(";")[0] for l in f.readlines()[3:]]
@@ -8,3 +12,19 @@ for v in diederich_vocab_raw:
         v = v.split(" ")[0]
     v = v.strip(punctuation)
     diederich_vocab.append(v)
+
+
+LexiconOptions = namedtuple("LexiconOptions", "type n")
+    
+    
+class Lexicon:
+    def __init__(self, freq_table_path: str, options: LexiconOptions):
+        freq_table = pd.read_csv(freq_table_path, index_col=0)
+        freq_table = freq_table.sort_values(by=["count"], ascending=False)
+        if options.type == "freq":
+            self.lexicon = freq_table[:options.n]
+        elif options.type == "perc":
+            n = int(len(freq_table) * options.n)
+            self.lexicon = freq_table[:n]
+        else:
+            raise AssertionError(f"{options.type} is not a valid lexicon type")
