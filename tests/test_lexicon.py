39f852a3110d064c5ea286058db8896163ba8ef2
Tyler Kirby <tyler.kirby9398@gmail.com>
2022-01-10 11:03:26 -0600

    make unit tests work

--- tests/test_lexicon.py
+++ tests/test_lexicon.py
@@ -3,11 +3,11 @@ from modules import Lexicon, LexiconOptions
 
 def test_lexicon_freq():
     opts = LexiconOptions("freq", 3)
-    test_lex = Lexicon("test_freq_table.csv", opts)
+    test_lex = Lexicon("tests/test_freq_table.csv", opts)
     assert len(test_lex.lexicon) == 3
 
 
 def test_lexicon_perc():
     opts = LexiconOptions("perc", 0.3)
-    test_lex = Lexicon("test_freq_table.csv", opts)
+    test_lex = Lexicon("tests/test_freq_table.csv", opts)
     assert len(test_lex.lexicon) == 5
